9556f5ecfeeecbf6e007fb754be1995d14c345c7
Ed Kubiak <ek33450505@gmail.com>
2022-03-31 16:12:06 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-  };
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw err; // stops the execution of the code
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
+
+// module 9.1 code used to show how to capture user input
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
